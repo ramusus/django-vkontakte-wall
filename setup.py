300cf2e4c938132bab68b659461f92575305b5e8
ramusus <ramusus@gmail.com>
2013-02-20 21:12:39 +0530

    travis integration

--- setup.py
+++ setup.py
@@ -13,11 +13,7 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False, # because we're including media that Django needs
-    install_requires=[
-        'django-vkontakte-api==0.2.1',
-        'factory_boy',
-#       'django-ajax-selects',
-    ],
+    install_requires=[r.strip() for r in open('requirements.txt').readlines()],
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
