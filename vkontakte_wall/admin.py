75cb0ed0ed8594d56247c353402a926fec12802f
ramusus <ramusus@gmail.com>
2014-02-12 13:00:51 +0530

    made imports compatible with Django 1.6

--- vkontakte_wall/admin.py
+++ vkontakte_wall/admin.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
-from django.utils.text import truncate_words
+try:
+    from django.template.defaultfilters import truncatewords
+except ImportError:
+    from django.utils.text import truncate_words as truncatewords
 from vkontakte_api.admin import VkontakteModelAdmin, GenericRelationListFilter
 from models import Post, Comment
 
@@ -26,7 +29,7 @@ class PostListFilter(admin.SimpleListFilter):
         parent_value = request.REQUEST.get(self.parent_parameter_name)
         if parent_value:
             ct_value, id_value = parent_value.split(self.separator)
-            return [(str(instance.post_id), truncate_words(instance.post.text, 5)) for instance in model_admin.model.objects.order_by().filter(**{self.ct_field_name: ct_value, self.id_field_name: id_value}).distinct(self.field_name)]
+            return [(str(instance.post_id), truncatewords(instance.post.text, 5)) for instance in model_admin.model.objects.order_by().filter(**{self.ct_field_name: ct_value, self.id_field_name: id_value}).distinct(self.field_name)]
 
     def queryset(self, request, queryset):
         parent_value = request.REQUEST.get(self.parent_parameter_name)
