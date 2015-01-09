9c8d2b5f17d98394402cd4877a6af8e72a525cc2
ramusus <ramusus@gmail.com>
2015-01-09 14:31:55 +0700

    added migration

--- vkontakte_wall/migrations/0015_auto__del_comment__del_field_post_likes__del_field_post_comments__del_.py
+++ vkontakte_wall/migrations/0015_auto__del_comment__del_field_post_likes__del_field_post_comments__del_.py
@@ -3,6 +3,8 @@ from django.db import models
 from south.db import db
 from south.utils import datetime_utils as datetime
 from south.v2 import SchemaMigration
+
+
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
